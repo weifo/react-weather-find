1c47e40835dba17f9bd9fc9ed9bfc95bfd8a3e17
weifo <1641929622@qq.com>
2019-01-01 21:15:37 +0800

    Updates

---
+++ precache-manifest.2bf456925376ae6e3e56f7e50618f7fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "/react-weather-find/static/media/bg.b6796c73.jpg"
+  },
+  {
+    "revision": "87a2f4473d1c6d142c2f",
+    "url": "/react-weather-find/static/js/runtime~main.87a2f447.js"
+  },
+  {
+    "revision": "1986a50041897b45da5b",
+    "url": "/react-weather-find/static/js/main.1986a500.chunk.js"
+  },
+  {
+    "revision": "c92f8f8ac712be22a3e8",
+    "url": "/react-weather-find/static/js/1.c92f8f8a.chunk.js"
+  },
+  {
+    "revision": "1986a50041897b45da5b",
+    "url": "/react-weather-find/static/css/main.84f1f822.chunk.css"
+  },
+  {
+    "revision": "c92f8f8ac712be22a3e8",
+    "url": "/react-weather-find/static/css/1.d8247152.chunk.css"
+  },
+  {
+    "revision": "f4f1d204999b3ac1f6c6203d8714fe11",
+    "url": "/react-weather-find/index.html"
+  }
+];
